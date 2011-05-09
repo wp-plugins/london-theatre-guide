9fda354821c9ccc9c59c25b30333500450fec2f3
London Drum <London Drum@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2011-05-09 17:23:56 +0000

    git-svn-id: https://plugins.svn.wordpress.org/london-theatre-guide/trunk@382899 b8457f37-d9ea-0310-8a92-e5e31aec5664

---
+++ london-theatre.php
@@ -0,0 +1,31 @@
+<?php /*
+
+Plugin Name: London Theatre Guide
+Version: 1.0
+Plugin URI: http://www.londondrum.com/events/?cat=33
+Description: Displays a list of upcoming theatre shows in London. All of the big theatres are included, with everything from big West End plays to stage musicals and Shakespearean tragedies. 
+Author: London Drum
+Author URI: http://www.londondrum.com/
+
+Copyright (c) 2009
+Released under the GPL license
+http://www.gnu.org/licenses/gpl.txt
+
+    This file is part of WordPress.
+    WordPress is free software; you can redistribute it and/or modify
+    it under the terms of the GNU General Public License as published by
+    the Free Software Foundation; either version 2 of the License, or
+    (at your option) any later version.
+
+    This program is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+    GNU General Public License for more details.
+
+    You should have received a copy of the GNU General Public License
+    along with this program; if not, write to the Free Software
+    Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
+	
+*/
+
+?>
